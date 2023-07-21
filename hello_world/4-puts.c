09d1fad2f0fc7f149d423c082f2412c7dc5bd177
sunny200745 <sunny.200745@gmail.com>
2023-07-21 09:10:38 -0700

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /*
- * main - print "Programming is like building a multilingual puzzle, followed by a newline.
+ * main - print "Programming is like building a multilingual 
+ * puzzle, followed by a newline.
  * Return: Always 0.
  */
 int main(void)
